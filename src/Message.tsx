24efc8b962bf4364358c1e8df2fd68f497baa74a
AnnaRakusu <annawang1008@gmail.com>
2023-08-03 00:27:51 +0800

    Initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+//PascalCasing 駝峰式命名
+function Message() {
+    const name = "";
+
+    if(name)
+        return<h1>Hello {name}</h1>
+    // JSX: JavaScript XML
+    return <h1>Hello World</h1>;
+}  
+
+export default Message;
